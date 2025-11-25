06a8c504c88fe45a93f565b4ba38b99ae029869e
Nikita Singh <nikita.singh.10964@gmail.com>
2025-11-25 19:34:11 +0530

    auth-middlewares

--- learning/middlewares.js
+++ learning/middlewares.js
@@ -1,6 +1,29 @@
 const express = require('express');
 
+// auth middlewares
+const {userAuth, adminAuth} = require('./middlewares/auth');
+
 const app = express();
+
+app.use('/admin', adminAuth);
+
+app.get('/admin/getAllData', (req, res) => {
+    res.send("All Data Sent");
+});
+
+app.get('/admin/deleteData', (req, res) => {
+    res.send("Data Deleted");
+});
+
+app.post('/user/login', (req, res) => {
+    res.send("User logged in");
+});
+
+app.get('/user/data', userAuth, (req, res) => {
+    res.send(" firstName: Nikita")
+});
+
+//----------------------------------------------------------------------------------------------
 
 // multiple route handlers
 
