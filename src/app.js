dc4bdf61236398241cec2cdcddc1638f4a39b4b2
Nikita Singh <nikita.singh.10964@gmail.com>
2025-11-25 20:05:22 +0530

    wildcard error handling

--- src/app.js
+++ src/app.js
@@ -1,25 +1,38 @@
 const express = require('express');
-const {userAuth, adminAuth} = require('./middlewares/auth');
 
 const app = express();
 
-app.use('/admin', adminAuth);
-
-app.get('/admin/getAllData', (req, res) => {
-    res.send("All Data Sent");
+// best way to handle errors - try & catch
+app.get('/getUserData2', (req, res) => {
+    try {
+        // logic
+        throw new Error("dnfjhgjk");
+        res.send("User Data Sent");
+    } catch (err) {
+          res.status(500).send('Some Error Occurred, please try again later');
+    }
 });
 
-app.get('/admin/deleteData', (req, res) => {
-    res.send("Data Deleted");
+// ERROR HANDLING using wildcard - below-
+
+app.get('/getUserData', (req, res) => {
+    // logic that might trigger an error
+    throw new Error("dnfjhgjk"); // eg: database connection failure
+    res.send("User Data Sent");
 });
 
-app.post('/user/login', (req, res) => {
-    res.send("User logged in");
+// wildcard error handling - always keep it at the end
+app.use((err, req, res, next) => {
+    if (err) {
+        res.status(500).send('Something went wrong');
+    }
 });
 
-app.get('/user/data', userAuth, (req, res) => {
-    res.send(" firstName: Nikita")
-});
+
+
+
+
+
 
 app.listen(7777, (req, res) => {
     console.log("Server is listening on port 7777");
