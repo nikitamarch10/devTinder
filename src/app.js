c9c2650ec4deb1be548c4d87f2d1ea6dcb5ab87c
Nikita Singh <nikita.singh.10964@gmail.com>
2025-11-26 08:35:12 +0530

    GET APIs

--- src/app.js
+++ src/app.js
@@ -3,8 +3,7 @@ const express = require('express');
 const connectDB = require('./config/database');
 const User = require('./models/user');
 
-// not best way since server is already listening and what if DB connection fails, 
-// it is better to have DB connection before server starts listening
+// not best way since server is already listening and what if DB connection fails, it is better to have DB connection before server starts listening
 // require('./config/database');
 
 const app = express();
@@ -20,6 +19,69 @@ app.post('/signup', async (req, res) => {
         res.send("User added successfully");
     } catch (err) {
         res.status(400).send("Error saving the user " + err.message);
+    }
+});
+
+// Feed API
+app.get('/feed', async (req, res) => {
+    try {
+        const users = await User.find({});
+        res.send(users);
+    } catch (err) {
+        res.status(400).send("Something went wrong");
+    }
+});
+
+app.get('/user', async (req, res) => {
+    try {
+        const userEmail = req.body.emailId;
+
+        console.log(userEmail);
+        const users = await User.find({ emailId: userEmail });
+
+        if (users.length === 0) {
+            res.status(404).send("User not found");
+        } else {
+            res.send(users);
+        }
+    } catch (err) {
+        res.status(400).send("Something went wrong");
+    }
+});
+
+// Exploring findOne
+app.get('/findOne', async (req, res) => {
+    try {
+        const userEmail = req.body.emailId;
+        const user = await User.findOne({ emailId: userEmail });
+
+        if (!user) {
+            res.status(404).send("User not found");
+        } else {
+            res.send(user);
+        }
+    } catch (err) {
+        res.status(400).send("Something went wrong");
+    }
+});
+
+// Exploring findById
+app.get('/findById', async (req, res) => {
+    try {
+        const userId = req.body._id;
+
+        console.log(userId);
+
+        // findById(id) is equivalent to findOne({ _id: id })
+        const user = await User.findById(userId);
+        console.log(user);
+        if (!user) {
+            res.status(404).send("User not found");
+        } else {
+            res.send(user);
+        }
+    } catch (err) {
+        res.status(400).send("Something went wrong " + err.message);
     }
 });
 
