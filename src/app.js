c5c7f7cb378611cae187d5e6a4375de0cd8fee14
Nikita Singh <nikita.singh.10964@gmail.com>
2025-12-02 06:39:42 +0530

    API level validations

--- src/app.js
+++ src/app.js
@@ -64,11 +64,22 @@ app.delete('/user', async (req, res) => {
     }
 });
 
-app.patch('/user', async (req, res) => {
-    const userId = req.body.userId;
+app.patch('/user/:userId', async (req, res) => {
+    const userId = req.params?.userId;
     const data = req.body;
 
     try {
+        const ALLOWED_UPDATES = ["photoURL", "gender", "about", "age", "skills"];
+        const isUpdateAllowed = Object.keys(data).every((key) => ALLOWED_UPDATES.includes(key));
+        
+        if (!isUpdateAllowed) {
+            throw new Error("Update not allowed");
+        }
+
+        if (data?.skills.length > 10) {
+            throw new Error("Skills cannot be more than 10");
+        }
+
         const user = await User.findByIdAndUpdate({_id: userId}, data,
             {
                 returnDocument: "after",
